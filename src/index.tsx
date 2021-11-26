ff80dbc33c10a8534961c7ccbcb3299cb6d16ab3
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-27 00:43:19 +0100

    Add bootstrap imports

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/* Add bootstrap */
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
