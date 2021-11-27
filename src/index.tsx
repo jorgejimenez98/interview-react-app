6d745d36e11fa02774bb7325412bb8be7075760d
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-27 03:19:03 +0100

    Add redux provider to index.js

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 /* Add bootstrap */
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+/* Redux Imports */
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
