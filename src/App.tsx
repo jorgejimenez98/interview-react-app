878e51b4ae52defd9939ea2fa34001f3fef385a6
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-27 01:19:38 +0100

    Change app inital code for browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+// Components
+import { Header } from "./components";
+import { Home } from "./screens";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/home" render={(props) => <Home {...props} />} />
+          <Redirect from="/" to="/home" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
