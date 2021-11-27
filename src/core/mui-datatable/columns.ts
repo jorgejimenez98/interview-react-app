2f741ba1e12130ecb3be21d54c663834587e0c86
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-27 04:13:07 +0100

    Init id and name columns

---
+++ src/core/mui-datatable/columns.ts
@@ -0,0 +1,20 @@
+export const columns = [
+  {
+    name: "id",
+    label: "",
+    options: {
+      filter: false,
+      sort: false,
+      display: false,
+      viewColumns: false,
+    },
+  },
+  {
+    name: "name",
+    label: "Name",
+    options: {
+      filter: true,
+      sort: true,
+    },
+  },
+];
