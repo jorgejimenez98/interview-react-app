f69958c5973fd83327a8bbcb272a3faca995c9a1
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-27 03:47:00 +0100

    Init get data from redux and api

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootStateType } from "../redux/reducers/root.reducers";
+import { get_data_request } from "../redux/actions/data.actions";
+/* Components */
+import { Loader, Message } from "../components";
 
 function Home() {
-    return (
-        <div>
-            List goes Here
-        </div>
-    )
+  const dispatch = useDispatch();
+
+  const { loading, persons, error } = useSelector(
+    (state: RootStateType) => state.data
+  );
+
+  console.log("Persons", persons);
+
+  useEffect(() => {
+    dispatch(get_data_request());
+  }, [dispatch]);
+
+  return (
+    <React.Fragment>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message severity="error">{error}</Message>
+      ) : (
+        <React.Fragment>
+          <div className="text-center">All is ok for now</div>
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
 }
 
-export default Home
+export default Home;
