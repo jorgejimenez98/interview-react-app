06d1d835dc8b86e008985cbc9042d287665e7392
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-27 03:46:37 +0100

    Update success response on data saga

--- src/redux/sagas/data-saga/data.saga.ts
+++ src/redux/sagas/data-saga/data.saga.ts
@@ -1,19 +1,18 @@
 import axios from "axios";
 import { all, call, put, takeLatest } from "redux-saga/effects";
-import { Person } from "../../../models/person";
 import { get_data_error, get_data_success } from "../../actions/data.actions";
 import { DataActionTypes } from "../../actionTypes/data_action.types";
 
 const apiUrl =
   "https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json";
-const getPosts = () => axios.get<Person[]>(apiUrl);
+const getPosts = () => axios.get<{}>(apiUrl);
 
 function* getDataSaga() {
   try {
     const response = yield call(getPosts);
     yield put(
       get_data_success({
-        persons: response.data,
+        persons: response.data.Brastlewark,
       })
     );
   } catch (error) {
@@ -30,7 +29,7 @@ function* getDataSaga() {
 }
 
 function* dataSaga() {
-  yield all([takeLatest(DataActionTypes.ERROR, getDataSaga)]);
+  yield all([takeLatest(DataActionTypes.REQUEST, getDataSaga)]);
 }
 
 export default dataSaga;
