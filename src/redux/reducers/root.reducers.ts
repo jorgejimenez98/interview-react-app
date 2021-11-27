381383c36c2e548554e25ed95287cf5cd588b830
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-27 03:45:00 +0100

    Change root state type

--- src/redux/reducers/root.reducers.ts
+++ src/redux/reducers/root.reducers.ts
@@ -5,6 +5,6 @@ const rootReducer = combineReducers({
   data: dataReducer,
 });
 
-export type RootState = ReturnType<typeof rootReducer>;
+export type RootStateType = ReturnType<typeof rootReducer>;
 
 export default rootReducer;
