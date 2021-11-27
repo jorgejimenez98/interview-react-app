cf349c66762c59d3cbf07442680f2231a62ea176
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-27 21:22:26 +0100

    Update image modal components to mui-material

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -1,23 +1,32 @@
-import React from "react";
-import { Button, Dialog } from "@material-ui/core";
-import { DialogTitle, DialogContent, DialogActions } from "./modalExtra";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import { BootstrapDialogTitle, BootstrapDialog } from "./modalExtra";
 
-export default function ImageModal({ open, handleClose }) {
+export default function ImageModal({ open, handleClose, image }) {
   return (
-    <Dialog
+    <BootstrapDialog
       onClose={handleClose}
       aria-labelledby="customized-dialog-title"
       open={open}
     >
-      <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-        Modal title
-      </DialogTitle>
-      <DialogContent dividers>Picture goes Here</DialogContent>
+      <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
+        {image.name}
+      </BootstrapDialogTitle>
+      <DialogContent dividers>
+        <img
+          src={image.src}
+          alt={image.alt}
+          className="img-fluid img-thumbnail rounded"
+          style={{ width: "400px", height: "400px" }}
+        />
+      </DialogContent>
       <DialogActions>
-        <Button autoFocus onClick={handleClose} color="primary">
+        <Button autoFocus onClick={handleClose}>
           Close
         </Button>
       </DialogActions>
-    </Dialog>
+    </BootstrapDialog>
   );
 }
