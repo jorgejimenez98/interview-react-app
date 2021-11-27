4ec2c4c53ae9a9738811755433d9a521d3cec954
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-27 01:18:59 +0100

    Create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="text-center mt-5">
+      <h4 className="text-muted">List of People</h4>
+    </div>
+  );
+}
+
+export default Header;
