ae71c74463bc698daaed123a08a11ccbc37196ea
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-27 21:22:56 +0100

    Delete dialog actions and dialog content component

--- src/components/modalExtra/DialogContent.tsx
+++
@@ -1,8 +0,0 @@
-import { Theme, withStyles } from "@material-ui/core/styles";
-import MuiDialogContent from "@material-ui/core/DialogContent";
-
-export const DialogContent = withStyles((theme: Theme) => ({
-  root: {
-    padding: theme.spacing(2),
-  },
-}))(MuiDialogContent);
