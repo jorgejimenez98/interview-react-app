e93440a56e1ab8415b3f363ecd009109be910b2a
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-28 18:29:27 +0100

    Create custum number filters component

---
+++ src/core/ColumnComponents/DisplayFilterNumbers.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { FormGroup, FormLabel, TextField } from "@mui/material";
+
+function DisplayFilterNumbers({ type, filterList, onChange, index, column }) {
+  return (
+    <div>
+      <FormLabel>{type}</FormLabel>
+      <FormGroup row>
+        <TextField
+          label="min"
+          type="number"
+          variant="standard"
+          value={filterList[index][0] || ""}
+          onChange={(event) => {
+            filterList[index][0] = event.target.value;
+            onChange(filterList[index], index, column);
+          }}
+          style={{ width: "45%", marginRight: "5%" }}
+        />
+        <TextField
+          label="max"
+          type="number"
+          variant="standard"
+          value={filterList[index][1] || ""}
+          onChange={(event) => {
+            filterList[index][1] = event.target.value;
+            onChange(filterList[index], index, column);
+          }}
+          style={{ width: "45%" }}
+        />
+      </FormGroup>
+    </div>
+  );
+}
+
+export default DisplayFilterNumbers;
