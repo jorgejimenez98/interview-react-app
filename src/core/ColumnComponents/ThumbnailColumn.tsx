381ffd2b44d64225b263c440fd397f1923613e61
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-29 00:49:03 +0100

    Add material-ui makeStyles function

--- src/core/ColumnComponents/ThumbnailColumn.tsx
+++ src/core/ColumnComponents/ThumbnailColumn.tsx
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
-import Avatar from "@mui/material/Avatar";
-import Tooltip from "@mui/material/Tooltip";
+import { Avatar, Tooltip } from "@material-ui/core";
 import { ImageModal } from "../../components";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    large: {
+      width: theme.spacing(7),
+      height: theme.spacing(7),
+      cursor: "pointer",
+    },
+  })
+);
 
 export default function ThumbnailColumn({ src, id, name }) {
+  const classes = useStyles();
   const [openDialog, setOpenDialog] = useState(false);
 
   const handleDialogClose = () => {
@@ -19,12 +30,7 @@ export default function ThumbnailColumn({ src, id, name }) {
     <div>
       <div onClick={handleOpenDialog}>
         <Tooltip title="Look Image">
-          <Avatar
-            alt={image.alt}
-            src={image.src}
-            sx={{ width: 56, height: 56 }}
-            className="pointer"
-          />
+          <Avatar alt={image.alt} src={image.src} className={classes.large} />
         </Tooltip>
       </div>
 
