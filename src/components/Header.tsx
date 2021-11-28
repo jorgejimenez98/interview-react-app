1651044e3a4582449d5d1c927e493a39e9482425
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-28 17:54:06 +0100

    Change Header Text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,10 @@ import React from "react";
 function Header() {
   return (
     <div className="text-center mt-5">
-      <h4 className="text-muted">List of People</h4>
+      <h3>
+        <strong>Interview Frontend Test</strong>{" "}
+        <small>Jorge Jiménez Díaz</small>
+      </h3>
     </div>
   );
 }
