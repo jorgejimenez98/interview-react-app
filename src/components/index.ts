2f7bb1d4e232df2fedc3b577d058e7ec1191cfcf
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-28 16:08:53 +0100

    Add details modal component to index imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import Header from "./Header";
 import Loader from "./Loader";
 import Message from "./Message";
 import ImageModal from "./ImageModal";
+import DetailsModal from "./DetailsModal";
 
-export { Header, Loader, Message, ImageModal };
+export { Header, Loader, Message, ImageModal, DetailsModal };
