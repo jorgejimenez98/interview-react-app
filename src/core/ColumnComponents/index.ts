a8d66ef8c6dab31b8888bdf3c348d5d91f5bb8c2
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-28 16:10:05 +0100

    Add action columns to index imports

--- src/core/ColumnComponents/index.ts
+++ src/core/ColumnComponents/index.ts
@@ -1,4 +1,5 @@
 import ThumbnailColumn from "./ThumbnailColumn";
 import HairColumnColumn from "./HairColumnColumn";
+import ActionsColumn from "./ActionsColumn";
 
-export { ThumbnailColumn, HairColumnColumn };
+export { ThumbnailColumn, HairColumnColumn, ActionsColumn };
