68118e30034d5a39f900a47b9502aacdfc8c8569
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-29 02:03:25 +0100

    Add Scroll to top Component to mina APP

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 // Components
-import { Header } from "./components";
+import { Header, ScrollTop } from "./components";
 import { Home } from "./screens";
 
 function App() {
   return (
     <div className="container">
       <BrowserRouter>
+        <ScrollTop />
         <Header />
         <Switch>
           <Route path="/home" render={(props) => <Home {...props} />} />
